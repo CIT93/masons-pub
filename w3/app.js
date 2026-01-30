25ef915fb3dcb24a04aee34afc224ae2aa244553
MasonJSanchez <MJSanchez22@MY.SCCCD.EDU>
2026-01-30 07:00:11 +0000

    Step 3 Complete

--- w3/app.js
+++ w3/app.js
@@ -1,50 +1,25 @@
 console.log('Hello from app.js! Your JavaScript is connected and running!');
 
-// Imports all exported helpers from the form-handler module.
-import * as orderForm from './order-handler.js';
+// Import everything
+import * as orderHandler from './order-handler.js';
 
-// References the main carbon footprint form.
-const carbonFootprintForm = document.getElementById('carbonFootprintForm');
+// Select elements
+const orderForm = document.getElementById('order-form');
+const orderSummary = document.getElementById('order-summary');
 
-// References the input field for the number of household members.
-const householdMembersInput = carbonFootprintForm.querySelector('#householdMembers');
-
-// References the clear form button.
-const clearFormButton = document.getElementById('clearFormButton');
-
-// --- Part 2: Code handleFormSubmit and handleClearForm Functions ---
-
-// Handles the form submission event, preventing default page reload.
-const handleFormSubmit = function (event) {
+// Submit handler
+const handleOrderSubmit = function (event) {
   event.preventDefault();
-  console.log(event);
-  // Refactor variable name to be formData which is the returned object literal
-  const formData = formHandler.getFormInputs();
-  // Refactor console for object literal
-  console.log(`key of householdMember value of ${formData.householdMembers}`);
-  console.log(`key of homeSquareFootage value of ${formData.homeSquareFootage}`);
-  console.log(`key of isApartment value of ${formData.isApartment}`);
-  console.log(`key of dietType value of ${formData.dietType}`);
-  console.log(`key of foodPackaging value of ${formData.foodPackaging}`);
-  console.log(formData);
+
+  const orderData = orderHandler.getOrderInputs();
+
+  orderSummary.textContent = `Ordered ${orderData.qty} ${orderData.size} T-Shirt(s). Gift Wrap: ${orderData.giftWrap}`;
 };
 
-// Handles the clear form button click, resetting form fields.
-const handleClearForm = function () {
-  formHandler.clearForm();
-  //carbonFootprintForm.reset();
-  //householdMembersInput.value = 1;
-  console.log('Clear button clicked');
+// Initiate function
+const init = function () {
+  console.log('App Initialized');
+  orderForm.addEventListener('submit', handleOrderSubmit);
 };
 
-
-// Initializes the app by attaching event listeners after the DOM is ready.
-  const init = function () {
-  console.log('App initialized: DOM is ready! Try submitting the form or clearing it.');
-
-  carbonFootprintForm.addEventListener('submit', handleFormSubmit);
-  clearFormButton.addEventListener('click', handleClearForm);
-};
-
-// Runs init once the DOM content has fully loaded.
 document.addEventListener('DOMContentLoaded', init);
