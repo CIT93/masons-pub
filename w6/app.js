08b8405dbe0065599a486a292c6178f996fc6a89
MasonJSanchez <MJSanchez22@MY.SCCCD.EDU>
2026-02-20 07:15:16 +0000

    Step 1 Complete

---
+++ w6/app.js
@@ -0,0 +1,54 @@
+console.log("Hello from app.js! Your JavaScript is connected and running!");
+
+import * as orderHandler from "./order-handler.js";
+import * as priceCalculator from "./price-calculator.js";
+import * as resultsDisplay from "./results-display.js";
+import * as orderStorage from './order-storage.js';
+
+// Reference to main form element
+const orderForm = document.getElementById("order-form");
+
+// Reference to order summary
+const orderSummaryInfo =
+  document.getElementById("order-summary-info") ||
+  document.getElementById("order-summary");
+
+// Step 3: Array to store orders
+const orders = [];
+
+// Handles form submission event
+const handleOrderSubmit = function (event) {
+  event.preventDefault();
+
+  const orderData = orderHandler.getOrderInputs();
+  const calculatedPrice = priceCalculator.calculateTotal(orderData);
+
+  const newOrder = {
+    ...orderData,
+    ...calculatedPrice,
+    timestamp: new Date().toISOString(),
+  };
+
+  orders.push(newOrder);
+  orderStorage.saveOrders(orders);
+
+  resultsDisplay.showOrderResults(newOrder);
+
+  console.log(orders);
+};
+
+// Initializes function
+const init = function () {
+  console.log("App Initialized");
+  
+  const loadedOrders = orderStorage.loadOrders();
+
+  if (loadedOrders.length > 0) {
+    orders.push(...loadedOrders);
+    console.log("Orders loaded");
+  }
+
+  orderForm.addEventListener("submit", handleOrderSubmit);
+};
+
+document.addEventListener("DOMContentLoaded", init);
